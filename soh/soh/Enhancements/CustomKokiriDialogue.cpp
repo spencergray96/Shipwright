88015b7d0a920c3b4a8b35b05e74898d28ecbcd1
spencer gray <spencergray666@hotmail.com>
2026-02-06 02:05:30 -0800

    first poc text modification

---
+++ soh/soh/Enhancements/CustomKokiriDialogue.cpp
@@ -0,0 +1,37 @@
+/**
+ * CustomKokiriDialogue.cpp
+ * 
+ * Test modification: Replaces a Kokiri child's dialogue with custom text.
+ * This hooks text ID 0x1004 which is used by a Kokiri in Kokiri Forest.
+ * 
+ * Author: Spencer
+ * Created: 2026-02-06
+ */
+
+#include <soh/OTRGlobals.h>
+
+extern "C" {
+#include <variables.h>
+}
+
+// Text ID 0x1004 is used by ENKO_TYPE_CHILD_0 in Kokiri Forest
+#define TEXT_KOKIRI_CHILD_0 0x1004
+
+void BuildCustomKokiriMessage(uint16_t* textId, bool* loadFromMessageTable) {
+    CustomMessage msg = CustomMessage(
+        "Dazrin Tioseco... Is a buster!",
+        "Hey! Ich bin eine benutzerdefinierte Nachricht!\nSpencer war hier!",
+        "Salut! Je suis un message personnalise!\nSpencer etait ici!"
+    );
+    msg.AutoFormat();
+    msg.LoadIntoFont();
+    *loadFromMessageTable = false;
+}
+
+void CustomKokiriDialogue_Register() {
+    // Always enabled (condition = 1)
+    COND_ID_HOOK(OnOpenText, TEXT_KOKIRI_CHILD_0, 1, BuildCustomKokiriMessage);
+}
+
+// Auto-register this enhancement on startup
+static RegisterShipInitFunc initFunc(CustomKokiriDialogue_Register);
